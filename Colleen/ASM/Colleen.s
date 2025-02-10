416c2017beedf9dcbdecd88665b7fb5cd9cda4af
Daniel Rodriguez <daniel.rodriguezpueyo@telefonica.com>
2025-02-10 16:46:13 +0100

    Replantear colleen en asm

--- Colleen/ASM/Colleen.s
+++ Colleen/ASM/Colleen.s
@@ -1,17 +1,5 @@
+;; probamos primero un holamundo
+
 .section text
 
-global main
 
-;; This is a comment outside of program context
-
-main:
-    
-    
-
-
-.section data
-    msg db ".section text
-
-global main
-
-;; This is a comment outside of program context"
