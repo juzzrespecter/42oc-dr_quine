b988a6c71454b863928feb9c4c5a39ba5afc37ad
juzzrespecter <danisarp@gmail.com>
2025-02-21 19:05:50 +0100

    Finished Colleen ASM, rewrite Colleen.c

--- Colleen/ASM/Colleen.s
+++ Colleen/ASM/Colleen.s
@@ -1,18 +1,22 @@
-;; Comment in global scope
-section .data
-    fmt:  db "%2$c;; Comment in global scope%3$csection .data%3$c    fmt:  db %1$s%3$csection .text%3$cextern _printf%3$cglobal _start%3$cfunction:%3$c    push fmt%3$c    push 0x22%3$c    push 0xa%3$c    call printf%3$c_start:%3$cmain:%3$c;; comment inside main function scope%3$c    call function%3$c    xor rdi, rdi%3$c    mov rax, 60%3$c    syscall%2$c"
+; Comment in global scope
 section .text
 extern printf
-global _start
+global main
 func:
-    push fmt
-    push 0x22
-    push 0xa
-    call printf
-_start:
+    push rbp
+    lea rdi, [rel fmt]
+    mov rsi, 0xa
+    mov rdx, 0x22
+    lea rcx, [rel fmt]
+    xor rax, rax
+    call printf wrt ..plt
+    pop rbp
+    ret
 main:
-;; comment inside main function scope
+; comment inside main function scope
+    push rbp
     call func
-    xor rdi, rdi
-    mov rax, 60
-    syscall
+    pop rbp
+    ret
+section .rodata
+    fmt:  db "; Comment in global scope%1$csection .text%1$cextern printf%1$cglobal main%1$cfunc:%1$c    push rbp%1$c    lea rdi, [rel fmt]%1$c    mov rsi, 0xa%1$c    mov rdx, 0x22%1$c    lea rcx, [rel fmt]%1$c    xor rax, rax%1$c    call printf wrt ..plt%1$c    pop rbp%1$c    ret%1$cmain:%1$c; comment inside main function scope%1$c    push rbp%1$c    call func%1$c    pop rbp%1$c    ret%1$csection .rodata%1$c    fmt:  db %2$c%3$s%2$c%1$c"
--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-
-/* Comment outside program definition */
-
+/*
+  Comment outside program definition
+*/
 void additional_function()
 {
-    char *s1 = "#include <stdio.h>%c%c/* Comment outside program definition */%c%cvoid additional_function()%c{%c    char *s1 = %c%s%c;%c    char *s2 = %c%s%c;%c    char *s3 = %c%s%c;%c";
-    char *s2 = "%c    printf(s1,10,10,10,10,10,10,34,s1,34,10,34,s2,34,10,34,s3,34,10);%c    printf(s2,10,10,10,10,10,10);%c    printf(s3,10,10,10,10,10,10);%c}%c%c";
-    char *s3 = "int main()%c{%c    /* Comment inside main func. */%c    additional_function();%c    return (0);%c}%c";
-
-    printf(s1,10,10,10,10,10,10,34,s1,34,10,34,s2,34,10,34,s3,34,10);
-    printf(s2,10,10,10,10,10,10);
-    printf(s3,10,10,10,10,10,10);
+    char *s = "#include <stdio.h>%1$c/*%1$c  Comment outside program definition%1$c*/%1$cvoid additional_function()%1$c{%1$c    char *s = %2$c%3$s%2$c;%1$c    printf(s,'\n','%2$c',s);%1$c}%1$c%1$cint main()%1$c{%1$c    /*%1$c      Comment inside main func.%1$c    */%1$c    additional_function();%1$c    return (0);%1$c}%1$c";
+    printf(s,'\n','"',s);
 }
 
 int main()
 {
-    /* Comment inside main func. */
+    /*
+      Comment inside main func.
+    */
     additional_function();
     return (0);
 }
