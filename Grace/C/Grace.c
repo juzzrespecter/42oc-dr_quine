7abac5e12f119eff26850c1e5270724e899e020f
juzzrespecter <danisarp@gmail.com>
2025-02-26 22:14:59 +0100

    Grace.c done

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,3 +1,11 @@
-/* This is probably a comment */
-# define P "Hola, fui printeado a archivo"
-# define MAIN(x)int main(){int f=open("Grace_kid.c",1);write(f,P,28);}
+/*
+  This is probably a comment
+*/
+# include <stdio.h>
+# include <fcntl.h>
+
+# define CODE "/*%1$c  This is probably a comment%1$c*/%1$c# include <stdio.h>%1$c# include <fcntl.h>%1$c%1$c# define CODE %2$c%3$s%2$c%1$c# define PRINT(x,y,z,u) dprintf(u,x,y,z,x)%1$c# define MAIN() int main(){int f=open(%2$cGrace_kid.c%2$c,65.420);PRINT(CODE,0xa,0x22,f);}%1$c%1$cMAIN()"
+# define PRINT(x,y,z,u) dprintf(u,x,y,z,x)
+# define MAIN() int main(){int f=open("Grace_kid.c",65.420);PRINT(CODE,0xa,0x22,f);}
+
+MAIN()
