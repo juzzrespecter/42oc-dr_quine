68a1e738a2552935d533d28b55145e99ecc8c202
juzzrespecter <danisarp@gmail.com>
2025-03-05 20:41:39 +0100

    Colleen en asm sin printf

--- Colleen/ASM/Colleen.s
+++ Colleen/ASM/Colleen.s
@@ -1,22 +1,43 @@
 ; Comment in global scope
 section .text
-extern printf
 global main
-func:
-    push rbp
-    lea rdi, [rel fmt]
-    mov rsi, 0xa
-    mov rdx, 0x22
-    lea rcx, [rel fmt]
-    xor rax, rax
-    call printf wrt ..plt
-    pop rbp
-    ret
 main:
-; comment inside main function scope
-    push rbp
-    call func
-    pop rbp
-    ret
+      call func ; comment inside main function scope
+      mov rax, 60
+      xor rdi, rdi
+      syscall
+nl:   push rsi
+      lea  rsi, [rel snl]
+      mov  rdx, 6
+      mov  rax, 1
+      syscall
+      pop rsi
+      inc rsi
+      mov rax, 1
+      mov rdx, 1
+      jmp wfmt
+func: mov rax, 1
+      mov rdi, 1
+      lea rsi, [rel fmt]
+      mov rdx, 783
+      syscall
+      mov rax, 1
+      lea rsi, [rel snl]
+      mov rdx, 1
+      syscall
+      lea rsi, [rel fmt]
+wfmt: syscall
+      inc rsi
+      cmp byte [rsi], 10
+      je  nl
+      cmp byte [rsi], 0
+      jne wfmt
+      lea rsi, [rel send]
+fock:
+      mov rdx, 4
+      syscall
+      ret
 section .rodata
-    fmt:  db "; Comment in global scope%1$csection .text%1$cextern printf%1$cglobal main%1$cfunc:%1$c    push rbp%1$c    lea rdi, [rel fmt]%1$c    mov rsi, 0xa%1$c    mov rdx, 0x22%1$c    lea rcx, [rel fmt]%1$c    xor rax, rax%1$c    call printf wrt ..plt%1$c    pop rbp%1$c    ret%1$cmain:%1$c; comment inside main function scope%1$c    push rbp%1$c    call func%1$c    pop rbp%1$c    ret%1$csection .rodata%1$c    fmt:  db %2$c%3$s%2$c%1$c"
+    snl:  db 34,44,49,48,44,34
+    send: db 34,44,48,10
+    fmt:  db "; Comment in global scope",10,"section .text",10,"global main",10,"main:",10,"      call func ; comment inside main function scope",10,"      mov rax, 60",10,"      xor rdi, rdi",10,"      syscall",10,"nl:   push rsi",10,"      lea  rsi, [rel snl]",10,"      mov  rdx, 6",10,"      mov  rax, 1",10,"      syscall",10,"      pop rsi",10,"      inc rsi",10,"      mov rax, 1",10,"      mov rdx, 1",10,"      jmp wfmt",10,"func: mov rax, 1",10,"      mov rdi, 1",10,"      lea rsi, [rel fmt]",10,"      mov rdx, 783",10,"      syscall",10,"      mov rax, 1",10,"      lea rsi, [rel snl]",10,"      mov rdx, 1",10,"      syscall",10,"      lea rsi, [rel fmt]",10,"wfmt: syscall",10,"      inc rsi",10,"      cmp byte [rsi], 10",10,"      je  nl",10,"      cmp byte [rsi], 0",10,"      jne wfmt",10,"      lea rsi, [rel send]",10,"fock:",10,"      mov rdx, 4",10,"      syscall",10,"      ret",10,"section .rodata",10,"    snl:  db 34,44,49,48,44,34",10,"    send: db 34,44,48,10",10,"    fmt:  db ",0
