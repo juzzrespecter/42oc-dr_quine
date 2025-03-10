72b6937be0b7ae331f928f99bb541ef0ee55ac09
juzzrespecter <danisarp@gmail.com>
2025-03-10 11:29:17 +0100

    Planteamiento Colleen python

---
+++ Colleen/Python/Colleen.py
@@ -0,0 +1,3 @@
+# Comment outside program definition
+def func(): print("# Comment outside program definition",10,"def func(): printf(%s)",10,"if __name__ == \"__main__\": func() # Comment inside main func.")
+if __name__ == "__main__": func() # Comment inside main func.
