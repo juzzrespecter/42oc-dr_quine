8b3e772c0db57f7deeccfc073a3d366b714d65f2
juzzrespecter <danisarp@gmail.com>
2025-03-21 19:20:56 +0100

    Grace: MAIN -> FT

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -4,8 +4,7 @@
 # include <stdio.h>
 # include <fcntl.h>
 
-# define CODE "/*%1$c  This is probably a comment%1$c*/%1$c# include <stdio.h>%1$c# include <fcntl.h>%1$c%1$c# define CODE %2$c%3$s%2$c%1$c# define PRINT(x,y,z,u) dprintf(u,x,y,z,x)%1$c# define MAIN() int main(){int f=open(%2$cGrace_kid.c%2$c,65,420);PRINT(CODE,0xa,0x22,f);}%1$c%1$cMAIN()"
+# define CODE "/*%1$c  This is probably a comment%1$c*/%1$c# include <stdio.h>%1$c# include <fcntl.h>%1$c%1$c# define CODE %2$c%3$s%2$c%1$c# define PRINT(x,y,z,u) dprintf(u,x,y,z,x)%1$c# define FT() int main(){int f=open(%2$cGrace_kid.c%2$c,65,420);PRINT(CODE,0xa,0x22,f);}%1$cFT()"
 # define PRINT(x,y,z,u) dprintf(u,x,y,z,x)
-# define MAIN() int main(){int f=open("Grace_kid.c",65,420);PRINT(CODE,0xa,0x22,f);}
-
-MAIN()
+# define FT() int main(){int f=open("Grace_kid.c",65,420);PRINT(CODE,0xa,0x22,f);}
+FT()
