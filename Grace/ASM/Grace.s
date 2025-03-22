8f476166d594b6e8a20b953cb874909d4a482496
juzzrespecter <danisarp@gmail.com>
2025-03-22 17:44:52 +0100

    Fix grace.s

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -1,8 +1,8 @@
-%define DATA "%define DATA ",10,"%macro RODATA 0",10,"SECTION .rodata",10,"path: db 71,114,97,99,101,95,107,105,100,46,115,0",10,"data: db DATA,0",10,"nl:   db 34,44,49,48,44,34",10,"endlp:db 34,44,48",10,"%endmacro",10,"%macro FT 0",10,"SECTION .text",10,"global main",10,"main: mov rax, 2",10,"      lea rdi, [rel path]",10,"      mov rsi, 65",10,"      mov rdx, 420",10,"      syscall",10,"      mov rdi, rax",10,"      mov rax, 1",10,"      lea rsi, [rel data]",10,"      mov rdx, 13",10,"      syscall",10,"      mov rax, 1",10,"      lea rsi, [rel nl]",10,"      mov rdx, 1",10,"      syscall",10,"      lea rsi, [rel data]",10,"loop: mov rdx, 1",10,"      syscall",10,"      inc rsi",10,"      cmp byte [rsi], 10",10,"      je cnl",10,"      cmp byte [rsi], 0",10,"      jne loop",10,"      mov rax, 1",10,"      lea rsi, [rel endlp]",10,"      mov rdx, 3",10,"      syscall",10,"      mov rax, 1",10,"      lea rsi, [rel data]",10,"      add rsi, 13",10,"      mov rdx, 989",10,"      syscall",10,"      mov rax, 60",10,"      syscall",10,"      ret",10,"cnl:  push rsi",10,"      lea rsi, [rel nl]",10,"      mov rdx, 6",10,"      syscall",10,"      pop rsi",10,"      inc rsi",10,"      mov rax, 1",10,"      jmp loop",10,"RODATA",10,"%endmacro",10,"FT ; Obligaciones contractuales",0
+%define DATA "%define DATA ",10,"%macro RODATA 0",10,"SECTION .rodata",10,"path: db 71,114,97,99,101,95,107,105,100,46,115,0",10,"data: db DATA",10,"nl:   db 34,44,49,48,44,34",10,"endlp:db 34,44,48",10,"%endmacro",10,"%macro FT 0",10,"SECTION .text",10,"global main",10,"main: mov rax, 2",10,"      lea rdi, [rel path]",10,"      mov rsi, 65",10,"      mov rdx, 420",10,"      syscall",10,"      mov rdi, rax",10,"      mov rax, 1",10,"      lea rsi, [rel data]",10,"      mov rdx, 13",10,"      syscall",10,"      mov rax, 1",10,"      lea rsi, [rel nl]",10,"      mov rdx, 1",10,"      syscall",10,"      lea rsi, [rel data]",10,"loop: mov rdx, 1",10,"      syscall",10,"      inc rsi",10,"      cmp byte [rsi], 10",10,"      je cnl",10,"      cmp byte [rsi], 0",10,"      jne loop",10,"      mov rax, 1",10,"      lea rsi, [rel endlp]",10,"      mov rdx, 3",10,"      syscall",10,"      mov rax, 1",10,"      lea rsi, [rel data]",10,"      add rsi, 13",10,"      mov rdx, 987",10,"      syscall",10,"      mov rax, 60",10,"      syscall",10,"      ret",10,"cnl:  push rsi",10,"      lea rsi, [rel nl]",10,"      mov rdx, 6",10,"      syscall",10,"      pop rsi",10,"      inc rsi",10,"      mov rax, 1",10,"      jmp loop",10,"RODATA",10,"%endmacro",10,"FT ; Obligaciones contractuales",0
 %macro RODATA 0
 SECTION .rodata
 path: db 71,114,97,99,101,95,107,105,100,46,115,0
-data: db DATA,0
+data: db DATA
 nl:   db 34,44,49,48,44,34
 endlp:db 34,44,48
 %endmacro
@@ -38,7 +38,7 @@ loop: mov rdx, 1
       mov rax, 1
       lea rsi, [rel data]
       add rsi, 13
-      mov rdx, 989
+      mov rdx, 987
       syscall
       mov rax, 60
       syscall
